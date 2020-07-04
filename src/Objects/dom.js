bb128d0083c6cdf21ed498cdde05cfc1031113f4
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-07-04 13:44:39 -0500

    Removed rest of DOM elementes from scenes

--- src/Objects/dom.js
+++ src/Objects/dom.js
@@ -29,7 +29,14 @@ const Dom = (() => {
       }
     };
   }
-  return { createForm, addButtonFunctionality };
+
+  function createStory() {
+    const div = document.createElement('div');
+    div.classList.add('story');
+    div.innerHTML = `<p>HP: Hold on! Can you hear that?</br>RW: It seems like somebody is coming. Take out your wands! Get ready!</br>HG: Remember what AD said about the Dementors, they are everywhere! We will come across one or two for sure in our way.</br>HP: Yes, think about a good memory and get ready to use your Patronum anytime now.</br>HP: Guys...</br>HP: Guys, where are you?</br>DE: It seems like it is only you and us now, we have placed your friends out of the scene so that we can settle the score right now. Oh, I almost forgot, we brought friends with us!</br></br></br> <strong>Use a/w/d/s to move, 'j' to launch your Confundus spell, and 'i' to conjure your Patronus.</br>Confundus won't work on Dementors and the Patronum will have no effect on the dark wizards. Good luck!</strong></p>`;
+    return div;
+  }
+  return { createForm, addButtonFunctionality, createStory };
 })();
 
 export default Dom;
--- src/Scenes/StoryScene.js
+++ src/Scenes/StoryScene.js
@@ -1,6 +1,7 @@
 import 'phaser';
 import config from '../Config/config';
 import Button from '../Objects/Button';
+import Dom from '../Objects/dom';
 
 export default class StoryScene extends Phaser.Scene {
   constructor() {
@@ -16,9 +17,7 @@ export default class StoryScene extends Phaser.Scene {
   create() {
     this.add.image(600, 300, 'bg');
 
-    const div = document.createElement('div');
-    div.classList.add('story');
-    div.innerHTML = `<p>HP: Hold on! Can you hear that?</br>RW: It seems like somebody is coming. Take out your wands! Get ready!</br>HG: Remember what AD said about the Dementors, they are everywhere! We will come across one or two for sure in our way.</br>HP: Yes, think about a good memory and get ready to use your Patronum anytime now.</br>HP: Guys...</br>HP: Guys, where are you?</br>DE: It seems like it is only you and us now, we have placed your friends out of the scene so that we can settle the score right now. Oh, I almost forgot, we brought friends with us!</br></br></br> <strong>Use a/w/d/s to move, 'j' to launch your Confundus spell, and 'i' to conjure your Patronus.</br>Confundus won't work on Dementors and the Patronum will have no effect on the dark wizards. Good luck!</strong></p>`;
+    const div = Dom.createStory();
 
     this.add.dom(
       this.game.config.width + 50,
