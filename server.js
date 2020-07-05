198481ab6f399e5278c86fe2ad6f44d1dc31ef36
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-07-05 00:33:46 -0500

    Fixing deployment bug WIP

--- server.js
+++ server.js
@@ -6,16 +6,15 @@ var app = express();
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('src'));
+  app.use(express.static(path.join(__dirname, 'build')));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'project.bundle.js'));
+    res.sendFile(path.resolve(__dirname, 'index.html'));
   });
 }
 
-app.use(express.static(path.join(__dirname, 'build')));
 app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function () {
