a12cf09c5f15ed5e6fa2dbd2b3d11fc4db4d07d7
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-07-05 00:36:54 -0500

    Fixing deployment bug WIP

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'));
   });
